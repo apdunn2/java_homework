ea90811a3800f8831c86f4340b85dd4c806336f5
Andrew Dunn <andrew.p.dunn93@gmail.com>
2017-05-21 15:26:30 -0500

    Week 8 updated

---
+++ 2013-OOProgrammingWithJava-PART2/week8-week8_12.OnlineShop/src/Storehouse.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Andrew
+ */
+public class Storehouse {
+    
+    public void addProduct(String product, int price, int stock){
+        
+    }
+    
+}
